00d88701c648903db3191519ae01849afece8c07
Sergey Stepygin <stepygin.sergey@gmail.com>
2018-05-06 22:20:53 +0300

    продолжение домашней работы

---
+++ Java Core. Продвинутый уровень/JavaCoreLesson4/src/ru/geekbrains/java/core/lesson4/ChatClient.java
@@ -0,0 +1,55 @@
+package ru.geekbrains.java.core.lesson4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+/**
+ Создать окно для клиентской части чата: большое текстовое поле для отображения переписки
+ в центре окна. Однострочное текстовое поле для ввода сообщений и кнопка для отсылки
+ сообщений на нижней панели. Сообщение должно отсылаться либо по нажатию кнопки на
+ форме, либо по нажатию кнопки Enter. При «отсылке» сообщение перекидывается из нижнего
+ поля в центральное.
+ */
+
+public class ChatClient extends JFrame {
+    JTextField jtf;
+    JTextArea jta;
+
+
+    public ChatClient(){
+        setTitle("Client chat");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setBounds(400, 400, 600, 500);
+        setLayout(new BorderLayout());
+
+
+        JButton button = new JButton("Отправить");
+        button.addActionListener(e -> sendMessage());
+
+        jtf = new JTextField();
+        jtf.setColumns(40);
+        jtf.addActionListener(e -> sendMessage());
+
+
+        jta = new JTextArea();
+        jta.setEditable(false);
+        JScrollPane scroll = new JScrollPane(jta);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add(jtf, BorderLayout.CENTER);
+        panel.add(button, BorderLayout.EAST);
+
+        add(scroll,BorderLayout.CENTER);
+        add(panel, BorderLayout.SOUTH);
+        setVisible(true);
+    }
+
+
+    private void sendMessage(){
+        jta.append(jtf.getText() + "\n");
+        jtf.setText("");
+    }
+}
---
+++ Java Core. Продвинутый уровень/JavaCoreLesson4/src/ru/geekbrains/java/core/lesson4/Main.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.java.core.lesson4;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    ChatClient chat = new ChatClient();
+    }
+}
