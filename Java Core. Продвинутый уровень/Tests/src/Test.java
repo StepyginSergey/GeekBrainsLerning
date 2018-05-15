e5f6f2f0190e02a8f6509c0a53f860bc33b23bc0
stepygin <stepygin@btlab.ru>
2018-05-15 15:21:41 +0300

    Тестовый проект. Для отработки вопросов при тестировании.

---
+++ Java Core. Продвинутый уровень/Tests/src/Test.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by stepygin on 15.05.2018.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> m = new HashMap<>();
+        m.
+
+        try{
+            return;
+        }finally {
+            System.out.println("Final");
+        }
+    }
+}
