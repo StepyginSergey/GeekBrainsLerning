e73014de5d2ae78753ad74604a036ee6c1cc1197
stepygin <stepygin@btlab.ru>
2018-05-17 16:06:19 +0300

    Выполнено ДЗ к уроку 7

---
+++ Java Core. Продвинутый уровень/JavaCoreFXChat/src/ru/jchat/core/server/ClientHandler.java
@@ -0,0 +1,77 @@
+package ru.jchat.core.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler {
+    private Server server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String nick;
+
+    public String getNick() {
+        return nick;
+    }
+
+    public ClientHandler(Server server, Socket socket) {
+        try {
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            new Thread(() -> {
+                try {
+                    while (true) {
+                        String msg = in.readUTF();
+                        if (msg.startsWith("/auth ")) {
+                            String[] data = msg.split("\\s");
+                            String newNick = server.getAuthService().getNickByLoginAndPass(data[1],data[2]);
+                            if (newNick != null) {
+                                nick = newNick;
+                                server.subscribe(this);
+                                sendMsg("/authok" + " " + nick);
+                                break;
+                            } else {
+                                sendMsg("Неверный логин/пароль");
+                            }
+                        }
+                    }
+                    while (true) {
+                        String msg = in.readUTF();
+                        System.out.println(nick + ": " + msg);
+                        if (msg.equals("/end")) break;
+
+                        if(msg.startsWith("/w ")){
+                            String[] data = msg.split("\\s",3);
+                            server.personalMsg(data[1], nick + ": " + data[2], nick);
+                        }else{
+                            server.broadcastMsg(nick + ": " + msg);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    server.unsubscribe(this);
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
