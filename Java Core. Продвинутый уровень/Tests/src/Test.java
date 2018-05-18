29d7c1cf900cda115807f9153d86bae6c1961ce7
stepygin <stepygin@btlab.ru>
2018-05-18 10:29:20 +0300

    Добавлены вопросы из тестирования, требующие разбирательства

--- Java Core. Продвинутый уровень/Tests/src/Test.java
+++ Java Core. Продвинутый уровень/Tests/src/Test.java
@@ -10,10 +10,17 @@ public class Test {
         NavigableMap<String, Integer> map = new TreeMap<>();
         HashMap<String, Integer> map1 = new LinkedHashMap<>();
 
+
+        int[] asf = new int[10];
+        asf[20] = 10;
+        asf[5] = asf[2] / 0;
+
         try{
             return;
         }finally {
             System.out.println("Final");
         }
+
+
     }
 }
