60afc13aac5caf0f56d3cf473b75e2ee1d1a3d05
stepygin <stepygin@btlab.ru>
2018-07-04 13:53:02 +0300

    Добавлены класы и утилиты с четвертого урока

--- Разработка игр под Android/StarGameL/core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ Разработка игр под Android/StarGameL/core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -5,9 +5,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.stargame.base.Base2DScreen;
+import ru.geekbrains.stargame.base.Sprite;
+import ru.geekbrains.stargame.math.Rect;
+import ru.geekbrains.stargame.math.Rnd;
+import ru.geekbrains.stargame.sprite.Background;
+import ru.geekbrains.stargame.sprite.Star;
 
 /**
  * Экран меню
@@ -15,7 +22,11 @@ import ru.geekbrains.stargame.base.Base2DScreen;
 
 public class MenuScreen extends Base2DScreen {
 
-    Texture img;
+    private Background background;
+    private Texture bg;
+    private Star star;
+    private TextureAtlas atlas;
+
 
     public MenuScreen(Game game) {
         super(game);
@@ -24,22 +35,37 @@ public class MenuScreen extends Base2DScreen {
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
+        bg = new Texture("textures/bg.png");
+        background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        TextureRegion starRegion = atlas.findRegion("star");
+        star = new Star(starRegion, Rnd.nextFloat(-0.005f, 0.005f),  Rnd.nextFloat(-0.5f, -0.1f), 0.01f);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        update(delta);
+        draw();
+    }
+
+    public void update(float delta) {
+        star.update(delta);
+    }
+
+    public void draw() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, -0.5f, -0.5f, 1f, 1f);
+        background.draw(batch);
+        star.draw(batch);
         batch.end();
     }
 
     @Override
     public void dispose() {
-        img.dispose();
+        bg.dispose();
+        atlas.dispose();
         super.dispose();
     }
 
@@ -51,5 +77,12 @@ public class MenuScreen extends Base2DScreen {
     @Override
     public void touchUp(Vector2 touch, int pointer) {
         super.touchUp(touch, pointer);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        star.resize(worldBounds);
     }
 }
--- Разработка игр под Android/StarGameL/desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
+++ Разработка игр под Android/StarGameL/desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
@@ -8,6 +8,10 @@ import ru.geekbrains.stargame.game.Star2DGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+//		float aspect = 9f/16f;
+		float aspect = 3f/4f;
+		config.width = 450;
+		config.height = (int) (config.width / aspect);
 		new LwjglApplication(new Star2DGame(), config);
 	}
 }
