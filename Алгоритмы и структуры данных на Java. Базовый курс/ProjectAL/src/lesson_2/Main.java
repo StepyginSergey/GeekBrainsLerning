fde7b319cc23a6ad344427b186ca778bf418d83a
StepyginSergey <stepygin.sergey@gmail.com>
2018-11-30 18:58:18 +0300

    Добавил несколько методов в главный класс

--- Алгоритмы и структуры данных на Java. Базовый курс/ProjectAL/src/lesson_2/Main.java
+++ Алгоритмы и структуры данных на Java. Базовый курс/ProjectAL/src/lesson_2/Main.java
@@ -16,6 +16,8 @@ public class Main {
         System.out.println("2 - Создание Обычного массива");
         System.out.println("3 - Сортировка обычного массива методом выбора");
         System.out.println("4 - Сортировка обычного массива методом вставки");
+        System.out.println("5 - Бинарный поиск в сортированном массиве");
+        System.out.println("6 - Линейный поиск в массиве");
         System.out.println("0 - Выход");
 
         Scanner input = new Scanner(System.in);
@@ -24,6 +26,7 @@ public class Main {
             int number;
             switch (operation){
                 case 1:
+                    arraySort = new MySortedArrayList<>();
                     System.out.println("Введите размер создаваемого сортированного массива");
                     number = input.nextInt();
 
@@ -36,6 +39,7 @@ public class Main {
                     break;
 
                 case 2:
+                    array = new MyArrayList<>();
                     System.out.println("Введите размер создаваемого массива");
                     number = input.nextInt();
 
@@ -46,6 +50,7 @@ public class Main {
                     finish = System.currentTimeMillis();
                     time("Время на создание обычного массива", start, finish);
                     break;
+
                 case 3:
                     start = System.currentTimeMillis();
                     array.insertionSort();
@@ -56,9 +61,28 @@ public class Main {
                     start = System.currentTimeMillis();
                     array.selectionSort();
                     finish = System.currentTimeMillis();
-                    time("Время затраченное на сортировку методом Вставки", start, finish);
+                    time("Время затраченное на сортировку методом Выбора", start, finish);
                     break;
+
                 case 5:
+                    System.out.println("Введите число от 0 до " + arraySort.size() +  ", которое хотите найти:");
+                    number = input.nextInt();
+
+                    start = System.currentTimeMillis();
+                    arraySort.find(number);
+                    finish = System.currentTimeMillis();
+                    time("Бинарный поиск. Время на поиск", start, finish);
+                    break;
+
+                case 6:
+                    System.out.println("Введите число от 0 до " + arraySort.size() +  ", которое хотите найти:");
+                    number = input.nextInt();
+
+                    start = System.currentTimeMillis();
+                    array.find(number);
+                    finish = System.currentTimeMillis();
+                    time("Линейный поиск. Время на поиск", start, finish);
+                    break;
 
                 default: break;
 
--- Алгоритмы и структуры данных на Java. Базовый курс/ProjectAL/src/lesson_2/MainHomeWork.java
+++
@@ -1,5 +0,0 @@
-package lesson_2;
-
-public class MainHomeWork {
-     
-}
